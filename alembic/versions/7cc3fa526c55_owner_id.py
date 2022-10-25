d26039553b6e85a948a7af91caf770066e790172
Shabo <Rony.Shabo@charter.com>
2022-10-25 16:53:50 -0500

    full revisions

--- alembic/versions/7cc3fa526c55_owner_id.py
+++ alembic/versions/7cc3fa526c55_owner_id.py
@@ -17,8 +17,11 @@ depends_on = None
 
 
 def upgrade() -> None:
+    op.add_column('resume',sa.Column('owner_id',sa.Integer(),nullable=False)),
+    op.create_foreign_key('resume_user_fk',source_table = "resume",referent_table="users",local_cols=['owner_id'],remote_cols=['id'],ondelete="CASCADE")
     pass
 
-
 def downgrade() -> None:
+    op.drop_constraint('resume_user_fk',table_name="resume")
+    op.drop_column('resume','owner_id')
     pass
--- alembic/versions/8d48578e6ad1_resume_table.py
+++ alembic/versions/8d48578e6ad1_resume_table.py
@@ -17,8 +17,17 @@ depends_on = None
 
 
 def upgrade() -> None:
+    op.create_table('resume',
+        sa.Column('id',sa.String(),nullable=False,autoincrement=True),
+        sa.Column('title',sa.String(),nullable=False),
+        sa.Column('work_place',sa.String(),nullable=False),
+        sa.Column('skills',sa.String(),nullable=False),
+        sa.Column('time_of_work',sa.String(),nullable=False),
+        sa.Column('created_at',sa.TIMESTAMP(timezone=True)),
+    server_default=sa.text('now()'),nullable=False)
     pass
 
 
 def downgrade() -> None:
+    op.drop_table('resume')
     pass
