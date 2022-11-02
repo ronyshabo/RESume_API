d8bc60a091af65b2de103aa50da65eb6914ef0f7
Shabo <Rony.Shabo@charter.com>
2022-11-02 16:05:53 -0500

    last time

--- alembic/versions/8d48578e6ad1_resume_table.py
+++ alembic/versions/8d48578e6ad1_resume_table.py
@@ -17,7 +17,7 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table('resume',
+    op.create_table('my_resume',
         sa.Column('id',sa.INTEGER(),nullable=False,autoincrement=True),
         sa.Column('title',sa.String(),nullable=False),
         sa.Column('work_place',sa.String(),nullable=False),
@@ -30,5 +30,5 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
-    op.drop_table('resume')
+    op.drop_table('my_resume')
     pass
