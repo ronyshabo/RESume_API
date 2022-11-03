081526d30da40547d9a00c9889e2bb484e724828
Shabo <Rony.Shabo@charter.com>
2022-11-03 12:09:44 -0500

    correct user information #3

--- app/main.py
+++ app/main.py
@@ -14,12 +14,19 @@ app = FastAPI(
     title="Rony R. Shabo",
     description="""
     BackEnd Python developer
+    Source Code:        https://github.com/ronyshabo/RESume_API
+    Email:              ronyshabbo@gmail.com
+
     Welcome to my first API, I used my Resume as an example of the things I can perform 
-    in order to be able to view:
+    in order to be able to view my resume I encurage you to:
         1- Please start by creating a user with the POST command under Users.
-        2- Authorize your user "Log in"
-        3- Enter your email and password only in the green Authorize button to the right of the page
-        4- Explore it yourself, and most of all enjoy your time my friend
+        2- Authenticate your user "Log in" in the last endpoint. 
+        3- Enter only your email and password in the green Authorize button to the right of the page \
+        4- My changes are locked and nothing you do would change anything other users did.            \
+        5- Explore it yourself, and most of all enjoy your time my friend                              \
+        One last thing.                                                                                 \
+        6- Any changes you create would be added under your credentials.                                 ---->
+           so you will only be able to change / delete your entries. 
     """)
 origins=["*"]
 
