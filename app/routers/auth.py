e364ece927b3fbbf4d2eb80800267bd8538ff907
Shabo <Rony.Shabo@charter.com>
2022-11-08 14:22:59 -0600

    black linter

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,3 @@
-
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
@@ -7,9 +6,12 @@ from .. import db, schemas, models, utils, oauth2
 
 router = APIRouter(tags=["Authentication"])
 
-@router.post('/Login', response_model=schemas.Token)
-def login(user_credentials:OAuth2PasswordRequestForm = Depends(),
-        db: Session = Depends(db.get_db)):
+
+@router.post("/Login", response_model=schemas.Token)
+def login(
+    user_credentials: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(db.get_db),
+):
     """
     Purpose:
     validating the Token based on it's schema
@@ -18,11 +20,19 @@ def login(user_credentials:OAuth2PasswordRequestForm = Depends(),
     Access_Token / Bearer
     """
 
-    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
+    user = (
+        db.query(models.User)
+        .filter(models.User.email == user_credentials.username)
+        .first()
+    )
     if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,  detail=f"invalid access")
-    if not utils.verify(user_credentials.password,user.password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,  detail=f"invalid access")
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"invalid access"
+        )
+    if not utils.verify(user_credentials.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"invalid access"
+        )
 
-    access_token = oauth2.create_access_token(data= {"user_id":user.id})
-    return{"access_token":access_token, "token_type":"bearer"}
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
